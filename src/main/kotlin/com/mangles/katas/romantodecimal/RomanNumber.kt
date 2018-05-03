5b960873ad1bb50cf536003deeb28d34b17e2aba
manglestw <mangles@thoughtworks.com>
2018-05-03 14:07:48 +0200

    [Mireia] Substract value if next one is greater

--- src/main/kotlin/com/mangles/katas/romantodecimal/RomanNumber.kt
+++ src/main/kotlin/com/mangles/katas/romantodecimal/RomanNumber.kt
@@ -4,11 +4,19 @@ class RomanNumber {
 
     fun calculateDecimal(romanNumber: String): Int {
         var decimal = 0
-        for (romanSymbol in romanNumber){
-            decimal += toDecimal(romanSymbol.toString())
+        for (i in romanNumber.indices){
+            decimal += if (isSmallerThanNextSymbol(romanNumber, i)){
+                -toDecimal(romanNumber[i].toString())
+
+            }else {
+                toDecimal(romanNumber[i].toString())
+            }
         }
         return decimal
     }
+
+    private fun isSmallerThanNextSymbol(romanNumber: String, i: Int) =
+        toDecimal(romanNumber[i].toString()) < toDecimal(romanNumber.elementAtOrNull(i + 1).toString())
 
     fun toDecimal(romanNumber: String):Int{
         return when (romanNumber) {
